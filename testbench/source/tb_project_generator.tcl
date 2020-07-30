50f628583338be5efc3633f0dc26ef8205d2c2a6
wsicheng <sicheng@physics.ucsb.edu>
2020-07-30 16:00:10 -0700

    Change the structure of the repo

--- testbench/source/tb_project_generator.tcl
+++ testbench/source/tb_project_generator.tcl
@@ -7,19 +7,21 @@ set FPGA_TYPE xcku040-ffva1156-2-e
 # Generate ip
 set argv $FPGA_TYPE
 set argc 1
-# source ip_generator.tcl
+source ip_generator.tcl
 
 # Create project
 create_project tb_project ../tb_project -part $FPGA_TYPE -force
 set_property target_language VHDL [current_project]
 set_property target_simulator XSim [current_project]
 
-# Add files
+# Add testbench files
 add_files -norecurse "Firmware_pkg.vhd Firmware_tb.vhd ../ip/$FPGA_TYPE/clockManager/clockManager.xci ../ip/$FPGA_TYPE/ila/ila.xci ../ip/$FPGA_TYPE/lut_input1/lut_input1.xci ../ip/$FPGA_TYPE/lut_input2/lut_input2.xci"
-add_files "odmb/"
 add_files "vme/"
 add_files "dcfeb/"
 add_files -fileset constrs_1 -norecurse "Firmware_tb.xdc"
+
+# Add ODMB source code
+add_files "../../source"
 
 # Add tcl for simulation
 set_property -name {xsim.simulate.custom_tcl} -value {../../../../source/Firmware_tb.tcl} -objects [get_filesets sim_1]
